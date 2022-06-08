1ef5dd4a11ffdbd20236e71c5a02ad507c0f6b12
DESKTOP-M1Q0EBF\THIEN <thienphamuser@gmail.com>
2022-06-08 10:28:54 +0700

    change project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-
-</script>
-
 <template>
+
   <RouterView />
 </template>
 
-<style>
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { useStore } from 'vuex';
+import { computed } from '@vue/reactivity';
+
+const store = useStore();
+
+
+</script>
+
+<style lang="scss">
+* {
+  font-family: Poppins, sans-serif;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 </style>
---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import { createStore } from 'vuex';
+
+const state = {
+  counter: 1,
+};
+
+const mutations = {};
+
+const actions = {};
+
+const getters = {};
+
+const store = createStore({
+  state,
+  mutations,
+  actions,
+  getters,
+});
+
+export default store;
