d6891886180085f8f92d18398435ed4953ceba40
DESKTOP-M1Q0EBF\THIEN <thienphamuser@gmail.com>
2022-06-10 16:11:41 +0700

    working on ListIssuesView

--- src/views/ListIssuesView.vue
+++ src/views/ListIssuesView.vue
@@ -36,12 +36,37 @@
             <RouterLink :to="{ name: 'home' }" class="btn">
               <i class="fa-solid fa-file-import"></i>
             </RouterLink>
-            <RouterLink :to="{ name: 'home' }" class="btn">
+            <RouterLink :to="{ name: 'home' }" class="btn-group export-btn">
               <i class="fa-solid fa-file-export"></i>
+              <i class="fa-solid fa-chevron-down"></i>
             </RouterLink>
           </div>
           <button class="btn issue-btn ">Edit issues</button>
           <RouterLink :to="{ name: home }" class="issue-btn new-btn">New issus</RouterLink>
+        </div>
+      </div>
+      <div class="filtered-search-input-container">
+        <div class="issuable-search-container">
+          <div class="btn-group search-history">
+            <i class="fa-solid fa-clock-rotate-left"></i>
+            <i class="fa-solid fa-chevron-down"></i>
+          </div>
+          <div class="filtered-search-input">
+            <input type="text" name="" placeholder="Search or filter results">
+          </div>
+          <div class="btn btn-helper">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </div>
+        </div>
+        <div class="sort-dropdown-container">
+          <div class="btn-group">
+            <span>Priority</span>
+            <i class="fa-solid fa-chevron-down"></i>
+          </div>
+          <div class="btn btn-helper">
+            <i v-if="isAscOrder" class="fa-solid fa-arrow-down-wide-short"></i>
+            <i v-else class="fa-solid fa-arrow-up-short-wide"></i>
+          </div>
         </div>
       </div>
     </div>
@@ -49,9 +74,12 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { RouterLink } from 'vue-router';
+
 const route = useRoute();
+const isAscOrder = ref(true);
 
 
 </script>
@@ -136,10 +164,65 @@ const route = useRoute();
 
         :nth-child(2) {
           border-radius: 0px 5px 5px 0px;
+          border: 1px solid rgb(199, 199, 199);
           border-left: none;
+
+          i {
+            border: none;
+          }
         }
       }
     }
+  }
+}
+
+.filtered-search-input-container {
+  display: flex;
+  background-color: rgb(248, 248, 248);
+  border-bottom: 1px solid rgb(199, 199, 199);
+  gap: 1rem;
+  padding: 1.1rem;
+
+  .issuable-search-container {
+    display: flex;
+    background-color: white;
+    width: 100%;
+
+    .search-history {
+      width: 8%;
+
+      &:hover {
+        background-color: rgb(226, 226, 226);
+      }
+
+      :nth-child(1) {
+        font-size: 1.1rem;
+      }
+
+      :nth-child(2) {
+        font-size: .7rem;
+      }
+    }
+
+    .filtered-search-input {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 95%;
+      border: 1px solid grey;
+      padding: .5rem;
+      border-left: none;
+
+      input {
+        width: 100%;
+        border: none;
+        outline: none;
+      }
+    }
+  }
+
+  .sort-dropdown-container {
+    display: flex;
   }
 }
 </style>
